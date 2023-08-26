28e05d6c3f6d51d216082d5922202a637bc03aa5
pedrolucas201 <quizueiramusj@gmail.com>
2023-08-26 14:54:00 -0300

    Primeiro commit da agenda

--- src/TesteAgenda.java
+++ src/TesteAgenda.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Contato {
+public class TesteAgenda {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -9,8 +9,30 @@ public class Contato {
         String nome = scanner.nextLine();
 
         Agenda agenda = new Agenda(nome);
-        agenda.setContatos();
+        Contato[] contatos = new Contato[3];
+        for (int i = 0; i < 3; i++){
+            System.out.println("Escreva as informaçoes do contato " + (i+1));
+            Contato c = new Contato();
 
+            System.out.println("Digite o nome do contato: ");
+            nome = scanner.nextLine();
+            c.setNome(nome);
+
+            System.out.println("Digite o número do contato: ");
+            String telefone = scanner.nextLine();
+            c.setTelefone(telefone);
+
+            System.out.println("Digite o e-mail do contato: ");
+            String email = scanner.nextLine();
+            c.setEmail(email);
+
+            contatos[i] = c;
+        }
+        agenda.setContatos(contatos);
+
+        if (agenda != null){
+            System.out.println(agenda.obterInfo());
+        }
     }
 
 }
